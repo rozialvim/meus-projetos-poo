f120f354432e469eace874afd046b97c0bc58fa3
rozialvim <eebmp.rao@educacao.arcos.mg.gov.br>
2024-07-19 20:39:26 -0300

    Update BoasVindas.java

--- BoasVindas.java
+++ BoasVindas.java
@@ -1,9 +1,13 @@
-import java.util.scanner;
+import java.util.Scanner;
 public class BoasVindas {
     public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
-        String nome,sobrenome;
-        System.out.println("");
+        String nome, sobrenome;
+        System.out.println("Digite seu nome.");
+        nome = scanner.nextLine();
+        System.out.println("Digite seu sobrenome.");
+        sobrenome = scanner.nextLine()
+
     }
     
 }
