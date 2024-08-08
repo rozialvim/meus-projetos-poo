34feb37d69369bb526549e67b236a1e3adeee268
rozialvim <eebmp.rao@educacao.arcos.mg.gov.br>
2024-08-07 22:29:43 -0300

    Update CalculadoraGeometria.java

--- CalculadoraGeometria.java
+++ CalculadoraGeometria.java
@@ -1,3 +1,35 @@
+import java.util.Scanner;
+
+importjava.util.Scanner;
 public class CalculadoraGeometria {
+    public static void main(String[]args){
+        Scanner scanner = new scanner(System.in);
+        int menu = 0; 
+
+        while (menu== 0) {
+            System.out.println("Selecione a opção correspondente:");
+            System.out.println("\n1- Área do Círculo.");
+            System.out.println("2- Área do Retângulo.");
+            System.out.println("3- Área do Triângulo.");
+            System.out.println("4- Sair.");
+            System.out.println("\n->");
+            menu = scanner.nextInt();
+            switch (menu) {
+                case 1: círculo();
+                        break;
+                case 2: retângulo();
+                        break;
+                case 3: triângulo();
+                        break;
+                
+            
+                default:
+                    break;
+            }
+
+
+            
+        }
+    }
     
 }
