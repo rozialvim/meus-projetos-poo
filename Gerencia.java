8e9c4a284fd7126bfdf263adc639fecf48fcb1eb
rozialvim <eebmp.rao@educacao.arcos.mg.gov.br>
2024-08-19 20:52:23 -0300

    19/08

---
+++ Gerencia.java
@@ -0,0 +1,7 @@
+public class Gerencia {
+    public static void main(String[] args) {
+        String setor = "Gerencia";
+
+
+    }
+}
---
+++ Produção.java
@@ -0,0 +1,9 @@
+public class Produção {
+     String setor = "Produção";
+     public void verSetor(){
+        System.out.println("Você está no setor de produção: ");
+     }
+     private void verStatus(){
+        System.out.println("Este método é privado");
+     }
+}
