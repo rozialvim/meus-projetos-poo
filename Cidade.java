9871d92af59032215f62aed51699991e46b4890f
rozialvim <eebmp.rao@educacao.arcos.mg.gov.br>
2024-08-21 22:26:49 -0300

    cidade

--- Cidade.java
+++ Cidade.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 public class Cidade {
     public static void main(String[]args){
         Temperatura temperatura = new Temperatura();
-        int opcao, resultadoTemp;
+        int opcaoCidade, resultadoTemp;
         String nomeCidade;
-        Scanner scanner.new Scanner(System.in);
-        System.out.println("Digite a cidade que quer informação: ");
-        nomeCidade = scanner.nextLine();
-        while (opcao) {
+        Scanner scanner=new Scanner(System.in);
+       
+        menu(opcaoCidade) {
+            System.out.println("Selecione a cidade que quer saber a temperatura: ")
             System.out.println(" 1- Bangkok:");
             System.out.println(" 2- Dubai:");
             System.out.println(" 3- São Paulo:");
@@ -19,6 +19,33 @@ public class Cidade {
             System.out.println(" 9- Toquio:");
             System.out.println(" 10- Cairo:");
 
+            System.out.println("Digite a opcao: ");
+            opcaoCidade = scanner.nextInt();
+            scanner.close();
         }
+            switch (opcao) {
+                case 1:nomeCidade = "Bangkok";
+                    break;
+                case 2: nomeCidade = "Dubai";
+                    break;
+                case 3: nomeCidade = "São Paulo";
+                    break;
+                case 4: nomeCidade = "New York";    
+                    break;
+                case 5: nomeCidade = "Sydney";
+                    break;
+                case 6: nomeCidade = "Londres";
+                    break;
+                case 7: nomeCidade = "Moscou";
+                    break;
+                case 8: nomeCidade = "Cidade do México";
+                    break;
+                case 9: nomeCidade = "Tóquio";
+                    break;
+                case 3: nomeCidade = "Cairo";
+                    break;
+                default:
+                    break;
+            }
     }
 }
