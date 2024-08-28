f257056b9779bf49ec1ad418066289cfb93cb6ad
rozialvim <eebmp.rao@educacao.arcos.mg.gov.br>
2024-08-27 21:58:15 -0300

    Create Cachorro.java

---
+++ Cachorro.java
@@ -0,0 +1,9 @@
+public class Cachorro {
+  public static void main(String[]args){
+    String nome, raça, cor;
+    int idade;
+    double peso;
+    Scanner scanner = new Scanner(System.in);
+    System.out.,println(" Digite os dados do cachorro: ");
+  }  
+}
