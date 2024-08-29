517d73759d7bbbc35cd48d7d9006e3532c0165c3
rozialvim <eebmp.rao@educacao.arcos.mg.gov.br>
2024-08-28 22:28:30 -0300

    cachorro

--- Cachorro.java
+++ Cachorro.java
@@ -1,9 +1,42 @@
+import java.util.Scanner;
+
 public class Cachorro {
+  Scanner scanner = new Scanner(System.in);
   public static void main(String[]args){
     String nome, raça, cor;
-    int idade;
+    int idade,opcao;
     double peso;
-    Scanner scanner = new Scanner(System.in);
-    System.out.,println(" Digite os dados do cachorro: ");
-  }  
-}
+    System.out.println("CADASTRE SEU CACHORRO: ");
+    System.out.println("Nome: ");
+    nome = scanner.nextLine();
+    System.out.print("Raça: ");
+    raça = scanner.nextLine();
+    System.out.print("Idade: ");
+    idade = scanner.nextInt();
+    System.out.print("Peso: ");
+    peso = scanner.nextDouble();
+    System.out.print"Cor: ");
+    cor = scanner.nextline();
+    System.out.println("\n CACHORRO SALVO COM SUCESSO \n ");
+
+    System.out.println(" MENU DE AÇÕES DO CACHORRO ");
+    System.out.println(" O QUE VOCÊ QUE O CACHORRO FAÇA?: ");
+    System.out.println("1- Latir");
+    System.out.println("2- Comer");
+    System.out.println("3- Buscar Objeto");
+    System.out.println("4- Brincar");
+    System.out.println("5- Dormir");
+    System.out.println("0- Sair ");
+    System.out.println("opção Selecionada" );
+    opcao = scanner.nextInt();
+
+    
+
+
+  }
+
+}  
+    
+ 
+    
+
