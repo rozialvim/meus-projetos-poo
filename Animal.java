ae065bafd8773fae8362c2b46bd8e40ee6861ac5
rozialvim <eebmp.rao@educacao.arcos.mg.gov.br>
2024-09-30 20:59:57 -0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+    public static void main(string[]args){
+        String nome, especie,cor;
+        int idade; 
+    } 
+    Animal Rex = new Animal(){
+    Rex.nome = "Rex";
+    Rex.especie = cachorro;
+    Rex.cor = caramelo;
+    Rex.idade = 7 anos;
+    }
+    
+}
