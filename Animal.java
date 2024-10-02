8e50945d365f798c491064332be247bc462cb177
rozialvim <eebmp.rao@educacao.arcos.mg.gov.br>
2024-10-01 21:54:23 -0300

    animal

--- Animal.java
+++ Animal.java
@@ -1,13 +1,44 @@
 public class Animal {
-    public static void main(string[]args){
-        String nome, especie,cor;
-        int idade; 
-    } 
-    Animal Rex = new Animal(){
-    Rex.nome = "Rex";
-    Rex.especie = cachorro;
-    Rex.cor = caramelo;
-    Rex.idade = 7 anos;
+    String nome, especie,cor;
+    int idade; 
+public void  exibirDados(){
+    System.out.println("Nome:" + nome);
+    System.out.println("Espécie:" + espécie);
+    System.out.println("Cor:" + cor);
+    System.out.println("Idade:" + idade);
+    System.out.println("\n======================\n");
+    }
+ } 
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
     
 }
---
+++ Principal.java
@@ -0,0 +1,42 @@
+public class Principal {
+    public static void main (String[] args) {
+        Animal Rex = new Animal();
+        Rex.nome = "Rex";
+        Rex.espécie = "cachorro";
+        Rex.cor = "caramelo";
+        Rex.idade = "7";
+        Rex.exibirDados();
+
+        Animal Garfield = new Animal();
+        Rex.nome = "Garfield";
+        Rex.espécie = "gato";
+        Rex.cor = "amarelo malhado";
+        Rex.idade = "2";
+        Garfield.exibirDados();
+
+        Animal Frajola = new Animal();
+        Rex.nome = "Frajola";
+        Rex.espécie = "gato";
+        Rex.cor = "preto e branco";
+        Rex.idade = "3";
+        Frajola.exibirDados();
+
+        Animal Gloo = new Animal();
+        Rex.nome = "Gloo";
+        Rex.espécie = "peixe";
+        Rex.cor = "laranja e branco";
+        Rex.idade = "1";
+        Gloo.exibirDados();
+
+        Animal Bem-te-vi = new Animal();
+        Rex.nome = "bem-te-vi";
+        Rex.espécie = "passaro";
+        Rex.cor = "preto";
+        Rex.idade = "5";
+        Bem-te-vi.exibirDados();
+
+    }
+}                                                                                                                                     
+                                                                                                                                
+
+
