add1417b7199167da32e1774da45bed89f133654
rozialvim <eebmp.rao@educacao.arcos.mg.gov.br>
2024-10-02 23:02:47 -0300

    Atualizar o Principal.java

--- Principal.java
+++ Principal.java
@@ -2,37 +2,37 @@ public class Principal {
     public static void main (String[] args) {
         Animal Rex = new Animal();
         Rex.nome = "Rex";
-        Rex.espécie = "cachorro";
+        Rex.especie = "cachorro";
         Rex.cor = "caramelo";
-        Rex.idade = "7";
+        Rex.idade = 7;
         Rex.exibirDados();
 
         Animal Garfield = new Animal();
-        Rex.nome = "Garfield";
-        Rex.espécie = "gato";
-        Rex.cor = "amarelo malhado";
-        Rex.idade = "2";
+        Garfield.nome = "Garfield";
+        Garfield.especie = "gato";
+        Garfield.cor = "amarelo malhado";
+        Garfield.idade = 2;
         Garfield.exibirDados();
 
         Animal Frajola = new Animal();
-        Rex.nome = "Frajola";
-        Rex.espécie = "gato";
-        Rex.cor = "preto e branco";
-        Rex.idade = "3";
+        Frajola.nome = "Frajola";
+        Frajola.especie = "gato";
+        Frajola.cor = "preto e branco";
+        Frajola.idade = 3;
         Frajola.exibirDados();
 
         Animal Gloo = new Animal();
-        Rex.nome = "Gloo";
-        Rex.espécie = "peixe";
-        Rex.cor = "laranja e branco";
-        Rex.idade = "1";
+        Gloo.nome = "Gloo";
+        Gloo.especie = "peixe";
+        Gloo.cor = "laranja e branco";
+        Gloo.idade = 1;
         Gloo.exibirDados();
 
         Animal Bem-te-vi = new Animal();
-        Rex.nome = "bem-te-vi";
-        Rex.espécie = "passaro";
-        Rex.cor = "preto";
-        Rex.idade = "5";
+        Ben-te-vi.nome = "bem-te-vi";
+        Bem-te-vi.especie = "passaro";
+        Bem-te-vi.cor = "preto";
+        Bem-te-vi.idade = 5;
         Bem-te-vi.exibirDados();
 
     }
