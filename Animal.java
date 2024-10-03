e748e814c222be18cce9e3d96735ae6166f5ddb5
rozialvim <eebmp.rao@educacao.arcos.mg.gov.br>
2024-10-02 22:50:54 -0300

    Atualizar o Animal.java

--- Animal.java
+++ Animal.java
@@ -1,9 +1,9 @@
 public class Animal{
     String nome, especie,cor;
-    int idade; 
-public void  exibirDados(){
+    int idade;
+    public void  exibirDados(){
     System.out.println("Nome:" + nome);
-    System.out.println("Espécie:" + espécie);
+    System.out.println("Espécie:" + especie);
     System.out.println("Cor:" + cor);
     System.out.println("Idade:" + idade);
     System.out.println("\n======================\n");
@@ -39,6 +39,6 @@ public void  exibirDados(){
 
 
 
-    }
     
-}
+    
+
