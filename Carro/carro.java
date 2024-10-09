e76bd11136be056a9808a8b42a02c0db68ff73da
rozialvim <eebmp.rao@educacao.arcos.mg.gov.br>
2024-10-08 21:14:04 -0300

    objeto carro

--- Carro/carro.java
+++ Carro/carro.java
@@ -3,11 +3,18 @@ package Carro;
 public class carro {
     String marca, modelo,status;
     int ano;   
-    public void exibirDados(){
+    public void exibirCarrosVendidos(){
        System.out.println("Marca:" + marca);
        System.out.println("Modelo:" + modelo);
        System.out.println("Ano:" + ano);
        System.out.println("Status:" + status);
        System.out.println("\n======================\n");
-      }  
+   }
+   public void exibirCarrosDisponiveis(){
+      System.out.println("Marca:" + marca);
+      System.out.println("Modelo:" + modelo);
+      System.out.println("Ano:" + ano);
+      System.out.println("Status:" + status);
+      System.out.println("\n======================\n");
+   }   
 }
