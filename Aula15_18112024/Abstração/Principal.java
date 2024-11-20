d8ff98e66853d968cf3c110ee78c91119d6cc83f
rozialvim <eebmp.rao@educacao.arcos.mg.gov.br>
2024-11-19 22:13:16 -0300

    principal

---
+++ Aula15_18112024/Abstração/Principal.java
@@ -0,0 +1,11 @@
+package Abstração;
+
+public class principal {
+    public static void main(String[]args){
+       Carro onix = new Carro ("Chevrolet", "onix",2020,0);
+       Carro hb20 = new Carro ("Hyndai", "hb20",2022,0);
+       Carro tiggo = new Carro ("Caoa Cherry", "onix",2024,0);
+       Carro haval = new Carro ("GWM", "haval",2023,0);
+    }
+
+} 
